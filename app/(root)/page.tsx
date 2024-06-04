0595f1768f7f3fb8e518ad8b83afa2cd45c50496
saketh <wow04wow@gmail.com>
2024-06-04 12:07:06 +0530

    git commit -m "add dashboard frontend upto cards"

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,36 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import Rightsidebar from '@/components/Rightsidebar'
 import React from 'react'
 
 const home = () => {
+    const LoggedIn = { firstName : "Saketh", lastName : "Bharadwaj", email:"wow04wow@gmail.com"}
+
+
   return (
     <section className='home'>
         <div className='home-content'>
-            <div className='home-header'>
-                Welcome, Saketh
-            </div>
+            <header className='home-header'>
+                <HeaderBox
+                    type = 'greeting'
+                    title = 'Welcome to Horizon'
+                    user = { LoggedIn?.firstName || 'Guest' }
+                    subtext = 'Access and manage your account and transactions efficiently.'
+                />
+                <TotalBalanceBox 
+                    accounts = {[]}
+                    totalBanks = {1}
+                    totalCurrentBalance = {1250.35}
+                />
+               
+            </header>
+            RECENT TRANSACTIONS
         </div>
+        <Rightsidebar
+            user = {LoggedIn}
+            transactions = {[]}
+            banks = {[{currentBalance:123.50}, {currentBalance:500.50}]}
+        />
     </section>
   )
 }
